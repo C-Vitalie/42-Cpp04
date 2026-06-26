5fe4450fffa35a33224a9285f2960cb6e2f158b3
Vita <vitalie.cucuiet@gmail.com>
2026-06-26 23:04:15 +0200

    ex01 done

--- ex01/obj/WrongAnimal.d
+++
@@ -1,1 +0,0 @@
-obj/WrongAnimal.o: src/WrongAnimal.cpp include/WrongAnimal.hpp
--- ex01/obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.cpp include/Dog.hpp include/Animal.hpp \
- include/Brain.hpp include/Cat.hpp include/WrongCat.hpp \
- include/WrongAnimal.hpp
